656ecffcf1efddaadf6d4afb4e5c1509e017d601
imannig <zahranansaria@gmail.com>
2022-09-10 16:23:07 +0700

    chime: Elixir adapt
    Signed-off-by: imannig <zahranansaria@gmail.com>

--- aosp_chime.mk
+++ aosp_chime.mk
@@ -12,15 +12,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/chime/device.mk)
 
 # Inherit some common xdroidOSS stuff.
-$(call inherit-product, vendor/xdroid/config/common.mk)
-XDROID_MAINTAINER := frostg012
-XDROID_BOOT := 1080
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := xdroid_chime
+PRODUCT_NAME := aosp_chime
 PRODUCT_DEVICE := chime
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Elixir
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_AOSP_RECOVERY := true
+IS_PHONE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+ELIXIR_MAINTAINER := Zahran Ansaria
+ELIXIR_BUILD_TYPE := OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
